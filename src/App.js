8d212885e6a9a5d470f16fc2b0bf5ad6f59fc2b5
mark48853 <sathanalao@hotmail.com>
2022-07-02 01:02:52 +0700

    a

--- src/App.js
+++ src/App.js
@@ -31,37 +31,8 @@ const App = () => {
 
     liff.shareTargetPicker([
       {
-        "type": "flex",
-        "altText": "share",
-        "contents": {
-          "type": "bubble",
-          "hero": {
-            "type": "image",
-            "size": "full",
-            "aspectRatio": "3:2",
-            "aspectMode": "cover",
-            "url": "https://images2.imgbox.com/11/33/zNWcdCmr_o.png"
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "button",
-                "style": "primary",
-                "height": "sm",
-                "action": {
-                  "type": "uri",
-                  "label": "แอดเลย",
-                  "uri": "https://liff.line.me/1657084978-W5NaqyDN?refer="+ref
-                },
-                "color": "#E6564E"
-              }
-            ],
-            "flex": 0
-          }
-        }
+        "type" : "text",
+        "text" : "test"
       }
     ])
       .then(result => alert(result.status))
@@ -149,6 +120,7 @@ const App = () => {
     <main className="App">
       <section>
         {ref == '0' ? "please wait . . ." : {invitationReady}}
+        {uid == "กรุณาผูกไลน์กับspeedkubก่อน คุณสามารถผูกได้ที่เมนู" ? "กรุณาผูกไลน์กับspeedkubก่อน คุณสามารถผูกได้ที่เมนู" : ""}
       </section>
     </main>
   );
